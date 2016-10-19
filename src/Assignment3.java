2f72f19c1514fd315d3cb88e5d727db9e76fbbf8
avinashraz <avinashaninthaneni@gmail.com>
2016-10-19 19:14:19 +0530

    Assignment 3

---
+++ src/Assignment3.java
@@ -0,0 +1,25 @@
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.println(j);
+			}
+			System.out.println();
+		}
+		for(int i=5;i>0;i--)
+		{
+			for(int j=1;j<i;j++)
+			{
+				System.out.println(j);
+			}
+			System.out.println();
+		}
+	}
+
+}
